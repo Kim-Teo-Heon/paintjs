4689b33db163ad040659385d7902edf6c50f6487
Kim-Teo-Heon <xbox1720@naver.com>
2021-03-03 23:46:41 +0900

    JS : change color, line width

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const canvas = document.querySelector('#js-canvas');
 const ctx = canvas.getContext('2d');
+const colors = document.querySelectorAll('.color');
+const range = document.querySelector('#range');
+const mode_btn = document.querySelector('#btns__mode-btn');
 
 // Hard
 canvas.width = 700;
@@ -9,6 +12,7 @@ ctx.strokeStyle = '#2c2c2c';
 ctx.lineWidth = '2.5';
 
 let painting = false;
+let filling = false;
 
 function start_painting() {
     painting = true;
@@ -16,13 +20,6 @@ function start_painting() {
 
 function stop_painting() {
     painting = false;
-}
-
-if(canvas) {
-    canvas.addEventListener('mousemove', on_mouse_move);
-    canvas.addEventListener('mousedown', on_mouse_down);
-    canvas.addEventListener('mouseup',stop_painting);
-    canvas.addEventListener('mouseleave', stop_painting)
 }
 
 function on_mouse_move(e) {
@@ -43,4 +40,47 @@ function on_mouse_move(e) {
 
 function on_mouse_down() {
     start_painting();
+}
+
+function handle_color_click(e) {
+    const color = e.target.style.backgroundColor;
+    
+    ctx.strokeStyle = color;
+}
+
+function handle_range_change(e) {
+    const width = e.target.value;
+    console.log(width)
+    ctx.lineWidth = width; 
+}
+
+function handle_mode_click(e) {
+    if(filling) {
+        filling = false;
+        e.target.innerText = 'Fill';
+    } else {
+        filling = true;
+        e.target.innerText = 'Paint';
+    }
+}
+
+if(canvas) {
+    canvas.addEventListener('mousemove', on_mouse_move);
+    canvas.addEventListener('mousedown', on_mouse_down);
+    canvas.addEventListener('mouseup',stop_painting);
+    canvas.addEventListener('mouseleave', stop_painting);
+}
+
+if(colors) {
+    colors.forEach((color) => {
+        color.addEventListener('click',handle_color_click);
+    })  
+} 
+
+if(range) {
+    range.addEventListener('input', handle_range_change);
+}
+
+if(mode_btn) {
+    mode_btn.addEventListener('click',handle_mode_click);
 }
